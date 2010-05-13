9aba51d446af210f20baeda44d67c78f682296e7
Chad Granum <exodist7@gmail.com>
2010-05-12 18:53:19 -0700

    Presentation

---
+++ presentation/slides/026.pl
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+tests Simple {
+    my $self = shift;
+    ok( 1, "Passing" );
+    ok( 0, "Failing" );
+}
+
+
+
+
+tests 'Complicated' => (
+    method => sub { ... },
+    # Only report a failure
+    no_result => 1,
+    # Skip or todo an entire group
+    skip => $reason,
+    todo => $reason,
+);
+
+
+
+
+
+
+
+
+
+
