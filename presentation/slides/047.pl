8aafae54d7acc534269863b41dedd188dcf4c9c4
ben hengst <notbenh@cpan.org>
2010-05-20 12:13:45 -0700

    removed the my $self = shift from the slides

--- presentation/slides/047.pl
+++ presentation/slides/047.pl
@@ -3,27 +3,27 @@
 
 
 
+
+
+
 cases "Act the same in these cases" {
-    my $self = shift;
 
     case a {
-        my $self = shift;
         $self->reset_data( 'a' );
     }
     case b {
-        my $self = shift;
         $self->reset_data( 'b' );
     }
 
     tests {
-        my $self = shift;
         ok( $self->have_data, "have data" );
     }
     tests {
-        my $self = shift;
         ok( $self->works, "it works" );
     }
 }
+
+
 
 
 
